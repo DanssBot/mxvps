556e1028cc2ae6ec5714e0a7b7503c63f88a6321
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-06-14 13:49:55 -0500

    Update instalscript.sh

--- vps-mx-free/instalscript.sh
+++ vps-mx-free/instalscript.sh
@@ -24,8 +24,8 @@ AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCO
 }
 clear
  msg -bar2
- msg -ama "     [ VPS - MX - SCRIPT \033[1;97m ❌ MOD By @Kalix1 ❌\033[1;33m ]"
- echo -e  "\033[1;97m               EJECUTANDO ACTULIZADOR  \033[1;34m "
+ msg -ama "     [ ADMIN VPS - SCRIPT \033[1;97m  Bienvenido \033[1;33m ]"
+ echo -e  "\033[1;97m               EJECUTANDO ACTUALIZADOR  \033[1;34m "
  msg -bar2
 ## Script name
 SCRIPT_NAME=vpsmxup
